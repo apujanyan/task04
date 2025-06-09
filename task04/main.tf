2881eb2ef9e2a37a56c63236058605937b4ec180
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-09 22:37:07 +0400

    ...

--- task04/main.tf
+++ task04/main.tf
@@ -1,128 +1,128 @@
 resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = var.location
-    tags = var.tags
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = var.vnet_name
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    address_space = ["10.0.0.0/16"]
-    tags = var.tags
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  tags                = var.tags
 }
 
 resource "azurerm_subnet" "subnet" {
-    name = var.subnet_name
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.1.0/24"]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
-    name = var.public_ip_name
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method = "Static"
-    sku = "Basic"
-    domain_name_label = var.dns_label
-    tags = var.tags
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+  domain_name_label   = var.dns_label
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_group" "nsg" {
-    name = var.nsg_name
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_rule" "allow_ssh" {
-    name = "AllowSSH"
-    priority = 1001
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "22"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-    resource_group_name = azurerm_resource_group.rg.name
-    network_security_group_name = azurerm_network_security_group.nsg.name
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
 resource "azurerm_network_security_rule" "allow_http" {
-    name = "AllowHTTP"
-    priority = 1002
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "80"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-    resource_group_name = azurerm_resource_group.rg.name
-    network_security_group_name = azurerm_network_security_group.nsg.name
+  name                        = "AllowHTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
 resource "azurerm_network_interface" "nic" {
-    name = var.nic_name
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    ip_configuration {
-        name = "internal"
-        subnet_id = azurerm_subnet.subnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.pip.id
-    }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
 
-    tags = var.tags
+  tags = var.tags
 }
 
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-    network_interface_id = azurerm_network_interface.nic.id
-    network_security_group_id = azurerm_network_security_group.nsg.id
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-    name = var.vm_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location = var.location
-    size = var.sku_type
-    admin_username = "azureuser"
-    admin_password = var.vm_password
-    disable_password_authentication = false
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = var.location
+  size                            = var.sku_type
+  admin_username                  = "azureuser"
+  admin_password                  = var.vm_password
+  disable_password_authentication = false
 
-    network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
-    os_disk {
-        name = "cmaz-d4qm9uvw-mod4-osdisk"
-        caching = "ReadWrite"
-        storage_account_type = "Standard_LRS"
+  os_disk {
+    name                 = "cmaz-d4qm9uvw-mod4-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  tags = var.tags
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      host     = azurerm_public_ip.pip.ip_address
+      user     = "azureuser"
+      password = var.vm_password
     }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer = "ubuntu-24_04-lts"
-        sku = "server"
-        version = "latest"
-    }
-
-    tags = var.tags
-
-    provisioner "remote-exec" {
-        connection {
-          type = "ssh"
-          host = azurerm_public_ip.pip.ip_address
-          user = "azureuser"
-          password = var.vm_password
-        }
-
-        inline = [
-            "sudo apt-get update",
-            "sudo apt-get install -y nginx",
-            "sudo systemctl enable nginx",
-            "sudo systemctl start nginx"
-        ]
-    }
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
 }
